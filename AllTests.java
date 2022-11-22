389281c06cf05788b8dffb6f7de87884a51c1028
Karun <74659873+anonymous-atom@users.noreply.github.com>
2022-11-22 14:27:23 +0530

    Test suite

---
+++ AllTests.java
@@ -0,0 +1,10 @@
+package jUnitTestPackage;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ testAddNumbers.class, testAddStrings.class })
+public class AllTests {
+
+}
