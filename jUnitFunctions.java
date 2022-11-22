8108356df24f34372a7aab3516eb9ef40271451e
Karun <74659873+anonymous-atom@users.noreply.github.com>
2022-11-22 14:25:52 +0530

    Crteated Functions

---
+++ jUnitFunctions.java
@@ -0,0 +1,14 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	
+	
+	public int AddNumber(int x, int y ) {
+		return x + y;
+	}
+	
+	public String AddStrings(String a, String b) {
+		return a + b;
+	}
+	
+}
